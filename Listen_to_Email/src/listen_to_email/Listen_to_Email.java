89b35dbbdf6b506d2b2839aa6b2b86d54c01b70d
Ana-Maria <anamariatucaliuc@yahoo.com>
2019-03-19 22:05:49 +0100

    ==initial

---
+++ Listen_to_Email/src/listen_to_email/Listen_to_Email.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listen_to_email;
+
+/**
+ *
+ * @author ana-maria
+ */
+public class Listen_to_Email {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
