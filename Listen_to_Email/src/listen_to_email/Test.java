e22a2957889fe2b605c44380880d0e69abbc263a
Ana-Maria <anamariatucaliuc@yahoo.com>
2019-04-01 17:21:56 +0200

    essai == test voice rss

--- Listen_to_Email/src/listen_to_email/Test.java
+++ Listen_to_Email/src/listen_to_email/Test.java
@@ -5,7 +5,11 @@
  */
 package listen_to_email;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -27,13 +31,37 @@ public class Test {
         AudioPlayer.player.start(audioStream);
       //  String line;
      }
+    public static void playSound(String sound) {
+    try {
+        InputStream in = new FileInputStream(new File(sound));
+        AudioStream audio = new AudioStream(in);
+        AudioPlayer.player.start(audio);
+    } catch (Exception e) {}
+}
+    private static String getHTML_bis(String urlToRead) throws Exception {
+      StringBuilder result = new StringBuilder();
+      URL url = new URL(urlToRead);
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      String line;
+      while ((line = rd.readLine()) != null) {
+         result.append(line);
+      }
+      rd.close();
+       AudioStream audioStream;
+        audioStream = new AudioStream(conn.getInputStream());
+       AudioPlayer.player.start(audioStream);
+      return result.toString();
+}
     
     public static void main (String [] args) throws Exception{
         try{
         System.out.println("  == Debut Test VoiceRss ==   ");
-        getHTML("http://api.voicerss.org/?key=c35f7f01e1c44bdba53f1b7e457b9670&hl=fr-fr&scr="  
-               + URLEncoder.encode("Message pour tester l'appication VoiceRSS","UTF-8"));
-        System.out.println("  == Fin Test VoiceRss ==   ");
+      //  getHTML("http://api.voicerss.org/?key=c35f7f01e1c44bdba53f1b7e457b9670&hl=fr-fr&scr="  
+     //          + URLEncoder.encode("Message pour tester l'appication VoiceRSS","UTF-8"));
+     playSound("Rapport bic pour le devellopement durable ");   
+     System.out.println("  == Fin Test VoiceRss ==   ");
         }
         catch (Exception e){
             e.printStackTrace();
