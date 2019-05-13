1efe7637e04ba9d89f5d3788dfca5b0f9808a979
Mirana09 <mirana.rakotomavo09@gmail.com>
2019-05-13 17:48:46 +0200

    csv messages recus

--- Listen_to_Email/src/listen_to_email/ConnexionGmail.java
+++ Listen_to_Email/src/listen_to_email/ConnexionGmail.java
@@ -9,13 +9,18 @@ package listen_to_email;
  * AuthentificationFailedExceptio;n Lien :
  * https://myaccount.google.com/lesssecureapps
  */
+import com.opencsv.CSVWriter;
 import com.voicerss.tts.AudioCodec;
 import com.voicerss.tts.AudioFormat;
 import com.voicerss.tts.Languages;
 import com.voicerss.tts.VoiceParameters;
 import com.voicerss.tts.VoiceProvider;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.*;
@@ -144,7 +149,7 @@ public class ConnexionGmail {
                         } 
                         resultat = resultat + "\n Texte : " + getTextFromMimeMultipart((MimeMultipart) multiPart);
 
-                        messages[i].setFlag(Flags.Flag.SEEN, true);   
+                        messages[i].setFlag(Flags.Flag.SEEN, true); 
                     }
                 }
                     System.out.println(resultat);
@@ -166,23 +171,33 @@ public class ConnexionGmail {
                 store.connect(host, username, motdepasse);
 
                 Folder inbox = store.getFolder("INBOX");
-                store.getDefaultFolder().list("*");
-                Folder sent_message = store.getFolder("");
+                //store.getDefaultFolder().list("*");
+                Folder sent_message = store.getFolder("SENT");
                 inbox.open(Folder.READ_WRITE);
 
                 int messageCount = inbox.getMessageCount();
 
+                Message[] lus = inbox.search(
+                        new FlagTerm(new Flags(Flag.SEEN), true));
+                
+                String expediteur = "";
+                for (int i = 0; i < lus.length; i++) {
+                    // messages[i].getFrom()[0];
+                    if (i==0) expediteur = expediteur + lus[i].getFrom()[0];
+                    else expediteur = expediteur + ";" + lus[i].getFrom()[0];
+                }
    
                 // Fetch unseen messages from inbox folder
-                Message[] messages = inbox.search(
+                Message[] non_lus = inbox.search(
                         new FlagTerm(new Flags(Flag.SEEN), false));
 
-
-                    for (int i = 0; i < messages.length; i++) {
-                      // messages[i].getFrom()[0];
-                     
-                        messages[i].setFlag(Flags.Flag.SEEN, true);   
-                    }
+                for (int i = 0; i < non_lus.length; i++) {
+                    // messages[i].getFrom()[0];
+                    expediteur = expediteur + ";" + non_lus[i].getFrom()[0];
+                    non_lus[i].setFlag(Flags.Flag.SEEN, true);
+                }
+                compterEmail(expediteur);
+                
                 inbox.close(true);
                 store.close();
             }
@@ -191,6 +206,66 @@ public class ConnexionGmail {
         }      
     }
 
+    public static void compterEmail (String text) throws IOException {
+      Hashtable table = new Hashtable();
+ 
+      StringTokenizer st;
+      String mot;
+      int nbOcc;
+ 
+ 
+	  st = new StringTokenizer(text, ";");
+	  while(st.hasMoreTokens())
+	    {
+	      mot = st.nextToken();
+	      if (table.containsKey(mot))
+		{
+		  nbOcc = ((Integer)table.get(mot)).intValue();
+		  nbOcc++;
+		}
+	      else nbOcc = 1;
+	      table.put(mot, new Integer(nbOcc));
+	    }
+
+                    // first create file object for file placed at location 
+        // specified by filepath 
+        File file = new File("messages_recus.csv");
+        try { 
+            // create FileWriter object with file as parameter 
+            FileWriter outputfile = new FileWriter(file); 
+
+            // create CSVWriter object filewriter object as parameter 
+            CSVWriter writer = new CSVWriter(outputfile, ',', CSVWriter.NO_QUOTE_CHARACTER); 
+
+            // adding header to csv 
+            /*String[] header = { "Expediteur", "Nombre de messages" }; 
+            writer.writeNext(header); */
+        
+          
+            Enumeration lesMots = table.keys();
+            while (lesMots.hasMoreElements())
+              {
+                mot = (String)lesMots.nextElement();
+                nbOcc = ((Integer)table.get(mot)).intValue();
+                String nb = Integer.toString(nbOcc);
+                System.out.println("Le mot " + mot + " figure " + nbOcc + " fois");
+
+                  // add data to csv 
+                  String[] data = { mot,nb }; 
+                  writer.writeNext(data); 
+
+                  // closing writer connection 
+                   
+              }
+            writer.close();
+          
+	}
+        catch (IOException e) { 
+            // TODO Auto-generated catch block 
+            e.printStackTrace(); 
+        }
+    }
+     
     public static void main(String[] args) throws Exception {
 
         final String host = "imap.gmail.com";
